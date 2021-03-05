1bdac97411c0566b1ce84c2fc2faa112c86633c1
ayakosomethingxxx@yahoo.co.jp <ayakosomethingxxx@gmail.com>
2021-03-05 22:39:21 +0900

    wip

---
+++ src/components/atoms/Timer.vue
@@ -0,0 +1,37 @@
+<template>
+  <button type="button" @click="onClick">スタート</button>
+  <div class="timer"><span class="fill"></span></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Timer',
+
+  emits: ['onClick'],
+
+  methods: {
+    onClick(): void {
+      this.$emit('onClick');
+    },
+  },
+};
+</script>
+
+<style>
+.timer {
+  width: 54px;
+  height: 65px;
+  background-image: url('../../assets/img/timer.svg');
+  position: relative;
+}
+
+.fill {
+  position: absolute;
+  border-radius: 50%;
+  bottom: 7px;
+  right: calc(50% - 20px);
+  background-image: conic-gradient(#d5525f 0% 25%, #fff 25% 100%);
+  width: 40px;
+  height: 40px;
+}
+</style>
