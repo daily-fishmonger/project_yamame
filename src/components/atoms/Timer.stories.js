05da268eba34111ef10d158413d84abaaaef1cd9
ayakosomethingxxx@yahoo.co.jp <ayakosomethingxxx@gmail.com>
2021-03-05 22:39:21 +0900

    wip

---
+++ src/components/atoms/Timer.stories.js
@@ -0,0 +1,11 @@
+import Timer from "./Timer.vue";
+
+export default {
+  title: "Atoms/Timer",
+  component: Timer,
+};
+
+export const Basic = () => ({
+  components: { Timer },
+  template: '<timer @onClick="onClick" />',
+});
