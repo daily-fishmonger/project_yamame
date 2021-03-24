6ca8ae1d8bc1dbc32393e94ed4dc8adeca9472fe
nisshii0313 <yuta19990313@gmail.com>
2021-03-24 22:20:04 +0900

    storybook内で共通スタイルを当てる

--- src/stories/index.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-
-import MyButton from './../components/MyButton.vue';
-
-storiesOf('MyButton', module).add('simple', () => ({
-  components: { MyButton },
-  template: '<MyButton>KEEP IT SIMPLE</MyButton>',
-}));
