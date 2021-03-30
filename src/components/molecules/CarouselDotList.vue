27813b9d7044241e599d262b6ee1052521bf213b
harukifumino <fu.miharun11@gmail.com>
2021-03-30 22:51:21 +0900

    molecules: CarouselDotListの追加

---
+++ src/components/molecules/CarouselDotList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <carousel-dot
+      v-for="n in countRange"
+      :key="n"
+      :is-active="n === activeCount"
+      class="carousel-dot"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import CarouselDot from '../atoms/CarouselDot.vue';
+
+export default defineComponent({
+  name: 'CarouselDotList',
+  components: {
+    CarouselDot,
+  },
+  props: {
+    totalCount: {
+      type: Number,
+      required: true,
+    },
+    activeCount: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const countRange = [...Array(props.totalCount).keys()].map((i) => i + 1);
+    return {
+      countRange,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.carousel-dot {
+  margin: 8px;
+}
+</style>
