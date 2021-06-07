c95c03d291f629891f3e51553f2a87ace194497d
ayakotanaka0224 <ayakosomethingxxx@gmail.com>
2021-06-07 22:06:59 +0900

    select dropdown

--- src/components/templates/StartResultFiBase.stories.ts
+++ src/components/templates/StartResultFiBase.stories.ts
@@ -1,25 +1,24 @@
 import StartResultBase from './StartResultBase.vue';
 import { Meta, Story } from '@storybook/vue3';
+import { Pages } from '../story';
 
 export default {
   title: 'Templates/StartResultBase',
   component: StartResultBase,
+  argTypes: {
+    page: {
+      control: {
+        type: 'select',
+        options: ['start', 'result'] as Pages[],
+      },
+    },
+  },
 } as Meta;
 
-const Basic: Story = (args) => ({
+export const Basic: Story = (args) => ({
   components: { StartResultBase },
   setup() {
     return { args };
   },
   template: '<start-result-base v-bind="args" />',
 });
-
-export const Start = Basic.bind({});
-Start.args = {
-  page: 'start',
-};
-
-export const Result = Basic.bind({});
-Result.args = {
-  page: 'result',
-};
