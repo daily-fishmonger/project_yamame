54cd0e9f5604638bf15d3febfc4de07c7c7d8ee3
fumiharun11 <fu.miharun11@gmail.com>
2021-09-17 23:24:31 +0900

    Playing画面のベース作成🍠

--- src/components/templates/Playing.vue
+++ src/components/templates/Playing.vue
@@ -27,19 +27,17 @@ export default defineComponent({
     Timer,
   },
   setup() {
-    // headerとfooterの高さを引いたものにする
-    const maxWidth = Math.min(window.innerWidth, 768);
-    const maxHeight = Math.min(window.innerHeight, 1024);
-    const game = new Game(
-      {
-        height: maxHeight,
-        width: maxWidth,
-      },
-      {
-        x: maxWidth / 2,
-        y: maxHeight / 2,
-      }
-    );
+    const maxWidth = Math.min(window.innerWidth, 732);
+    const maxHeight = Math.min(window.innerHeight, 898);
+    const canvasSize = {
+      height: maxHeight,
+      width: maxWidth,
+    };
+    const startPos = {
+      x: maxWidth / 2,
+      y: maxHeight / 2,
+    };
+    const game = new Game(canvasSize, startPos);
     onMounted((): void => {
       const gameArea = document.getElementById('gameArea');
       if (!gameArea) return;
@@ -63,11 +61,7 @@ export default defineComponent({
 
     const pauseButtonAction = (): void => {
       _toggleIsActive();
-      if (game.isPaused) {
-        _restart();
-      } else {
-        _pause();
-      }
+      return game.isPaused ? _restart() : _pause();
     };
 
     return {
@@ -81,10 +75,6 @@ export default defineComponent({
 <style scoped>
 @import url('../../assets/css/reset.css');
 
-canvas {
-  display: block;
-  margin: auto;
-}
 .game {
   background-color: #96c3ed;
   height: 1024px;
@@ -94,19 +84,15 @@ canvas {
 .header {
   display: flex;
   flex-direction: row;
-  justify-content: center;
   align-items: center;
-  padding: 0;
+  justify-content: space-between;
+  padding-bottom: 12px;
+  min-height: 78px;
 }
 .game-area {
   display: block;
-  margin: 12px auto;
-  background-color: white;
   border-radius: 12px;
   height: 1024px;
   width: 768px;
-}
-.fish {
-  margin: 0 58px;
 }
 </style>
